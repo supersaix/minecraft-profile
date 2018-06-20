3e00943f6c7009faf918980707dce8cec810bd24
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-06-20 17:19:56 -0600

    Remove better foliage :(

--- Compile-ModSource.ps1
+++ Compile-ModSource.ps1
@@ -1,1 +1,26 @@
 
+Param(
+    [switch]$Init
+);
+
+$Output = [Environment]::NewLine + "JAVA_HOME set to: ${Env:JAVA_HOME}" + [Environment]::NewLine
+Write-Host $Output -ForegroundColor Green
+
+If ($Init) {
+
+    Set-Location ".\source\"
+    Get-ChildItem -Directory | ForEach-Object {
+        Write-Host "Initializing" $_
+        Set-Location $_
+        $InitProcess = Start-Process -PassThru -NoNewWindow .\gradlew setupDecompWorkspace
+        $InitProcess.WaitForExit()
+        $InitProcess = Start-Process -PassThru -NoNewWindow .\gradlew eclipse
+        $InitProcess.WaitForExit()
+        Set-Location ".."
+        Write-Host "Complete Init for" $_
+    }
+
+    Set-Location ".."
+
+    Write-Host "Finished Init" -ForegroundColor Green
+}
