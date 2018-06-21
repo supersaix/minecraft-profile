74ea7f710178487a7993caa7aea7f9ed37b2b118
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-06-21 16:38:46 -0600

    gitignore update and script work

--- Compile-ModSource.ps1
+++ Compile-ModSource.ps1
@@ -1,8 +1,11 @@
 
 Param(
     [switch]$Init,
-    [string]$Build
+    [string]$Build = "false"
 );
+
+$IgnoreDirectories = @(".metadata",".recommenders")
+$IgnoreFiles = @("-sources", "-deobf", "-javadoc")
 
 $Output = [Environment]::NewLine + "JAVA_HOME set to: ${Env:JAVA_HOME}" + [Environment]::NewLine
 Write-Host $Output -ForegroundColor Green
@@ -27,14 +30,66 @@ If ($Init) {
 }
 
 Else {
+    Write-Host "Initializing process dictionary..."
+    $Processes = New-Object 'System.Collections.Generic.Dictionary[int, System.Diagnostics.Process]'
+    $Builds = New-Object 'System.Collections.Generic.Dictionary[int, string]'
+
+    Write-Host "Entering .\source\ to begin builds..."
     Set-Location ".\source\"
+    [int]$Counter = 1
     Get-ChildItem -Directory | ForEach-Object {
-        If ($_.Name -eq $Build) {
-            Write-Host "`nBuilding" $_
+
+        If (!$IgnoreDirectories.Contains($_.Name)) {
+            Write-Host "`nPreparing build" $_
             Set-Location $_
-            Start-Process -PassThru -NoNewWindow .\gradlew build
+            $Processes.Add($Counter, (Start-Process -RedirectStandardOutput buildlog.txt -PassThru -NoNewWindow .\gradlew build))
+            $Builds.Add($Counter, $_.Name)
             Set-Location ".."
+        } Else {
+            $Output = "`n" +  $_.Name + " is an ignored directory."
+            Write-Host $Output
         }
+        
+        If ( ($Counter -ge 2) -And ($Counter % 2 -eq 0) ) {
+            Write-Host "${Counter} is set, waiting for builds to complete"
+
+            Write-Host "Awaiting" $Builds[$Counter]
+            $Processes[$Counter].WaitForExit()
+            $Path = $PSScriptRoot + "\source\" + $Builds[$Counter] + "\build\libs"
+            Write-Host $Path
+            Get-ChildItem -Path $Path | ForEach-Object {
+                if ($_.Name.Contains($IgnoreFiles[0]) -Or $_.Name.Contains($IgnoreFiles[1]) -Or $_.Name.Contains($IgnoreFiles[2])) {
+                    Write-Host "Ignored file" $_
+                } Else {
+                    Copy-Item -Path $_.FullName -Destination "${PSScriptRoot}\builds" -Recurse -Force
+                }
+            }
+
+            Write-Host "Awaiting" $Builds[$Counter - 1]
+            $Processes[$Counter - 1].WaitForExit()
+            $Path = $PSScriptRoot + "\source\" + $Builds[$Counter - 1] + "\build\libs"
+            Write-Host $Path
+            Get-ChildItem -Path $Path | ForEach-Object {
+                if ($_.Name.Contains($IgnoreFiles[0]) -Or $_.Name.Contains($IgnoreFiles[1]) -Or $_.Name.Contains($IgnoreFiles[2])) {
+                    Write-Host "Ignored file" $_
+                } Else {
+                    Copy-Item -Path $_.FullName -Destination "${PSScriptRoot}\builds" -Recurse -Force
+                }
+            }
+        }
+        
+        If (!$IgnoreDirectories.Contains($_.Name)) {
+            $Counter += 1
+        }
+
     }
+    Write-Host "`nExiting .\source\, attempted to trigger all builds." -ForegroundColor Green
+
+    $Processes.Values | ForEach-Object {
+        $_.WaitForExit()
+    }
+
+    Write-Host "All build processes have resolved."
+
     Set-Location ".."
 }
--- Deploy-ModSource.ps1
+++ Deploy-ModSource.ps1
@@ -7,11 +7,14 @@ Get-ChildItem -Path $ModsDirectory | ForEach-Object {
     Write-Host $Output
 }
 
+Write-Host "`nDeploying mods...`n"
+Copy-Item -Path $ModsDirectory -Destination "${Env:APPDATA}\.minecraft\" -Recurse -Force
+
 Write-Host $ConfigDirectory -ForegroundColor Yellow
 Get-ChildItem -Path $ConfigDirectory | ForEach-Object {
     $Output = "`t\" + $_
     Write-Host $Output
 }
 
+Write-Host "`nDeploying configs...`n"
 Copy-Item -Path $ConfigDirectory -Destination "${Env:APPDATA}\.minecraft\" -Recurse -Force
-Copy-Item -Path $ModsDirectory -Destination "${Env:APPDATA}\.minecraft\" -Recurse -Force
